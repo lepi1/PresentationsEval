7bb85078c02f2d1ce9c597e4f0817e26f67a3627
lepi1 <tomaslepicg@gmail.com>
2020-02-26 20:15:13 +0100

    Show voting results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,6 @@
 class ResultsController < ApplicationController
     def index
         @votes = Vote.all
+        @votes_count_table = Vote.find_by_sql ["SELECT result, COUNT(*) AS num FROM votes GROUP BY result"]
     end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,3 @@
 class Vote < ApplicationRecord
-    @boring_count = nil
-    @liked_count = nil
-    @lack_of_details_count = nil
-    @lot_of_details_count = nil
+
 end
